22be17a49b879c80d358a746c9ed9feeddd9bc31
Éric Massé <Ericmas001@gmail.com>
2016-06-11 13:17:22 -0400

    SimpleTcpClient SimpleTcpServer RemoteTcpEntity

---
+++ Com.Ericmas001.Communication/RemoteTcpEntity.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Com.Ericmas001.Communication
+{
+    public abstract class RemoteTcpEntity
+    {
+        public TcpClient RemoteEntity { get; private set; }
+        public StreamReader m_Input;
+        public StreamWriter m_Output;
+        public RemoteTcpEntity(TcpClient remoteEntity)
+        {
+            RemoteEntity = remoteEntity;
+            m_Input = new StreamReader(remoteEntity.GetStream());
+            m_Output = new StreamWriter(remoteEntity.GetStream());
+            m_Output.AutoFlush = true;
+        }
+
+        // The core server task
+        public Task Run()
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                    OnDataReceived(await m_Input.ReadLineAsync());
+            });
+        }
+
+        protected abstract void OnDataReceived(string data);
+
+        protected abstract void OnDataSent(string data);
+
+        public void Send(string data)
+        {
+            try
+            {
+                Task.Run(async () => await m_Output.WriteLineAsync(data)).Wait();
+                OnDataSent(data);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
+
